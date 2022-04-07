c6bfa325f6586827b5aa69e01a8c0977b97e0ce6
cme2me <max.zhakov@gmail.com>
2022-04-07 12:33:58 +0300

    first 2 tests done

---
+++ src/test/java/Testing.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Testing {
+    @Test
+    public void test1() {
+        int[] in = new int[] {1,3,4,5,5,6,8};
+        int[] out = new int[] {5,5,6,8};
+        Assertions.assertArrayEquals(out, Main.checkingArr(in));
+    }
+    @Test
+    public void test2() {
+        int[] in = new int[]{1,3,5,5,6,8};
+        Assertions.assertThrows(RuntimeException.class, ()->{
+            Main.checkingArr2(in);
+        });
+    }
+}
